667d3d4b5a0aa372bc6df8d177688d6025cb23c5
raphaelernani <raphaelernani@localhost>
2013-04-20 17:03:43 -0300

    Optimizing AliasSets pass to have a linear asymptotic complexity

--- src/AliasSets/AliasSets.h
+++ src/AliasSets/AliasSets.h
@@ -5,6 +5,7 @@
 #include "llvm/Analysis/AliasSetTracker.h"
 #include "llvm/Analysis/AliasAnalysis.h"
 #include "llvm/ADT/ilist.h"
+#include "llvm/ADT/DenseMap.h"
 #include "llvm/BasicBlock.h"
 #include "llvm/Function.h"
 #include "llvm/Module.h"
@@ -19,23 +20,33 @@
 #include<map>
 #include<queue>
 
-class AliasSets : public ModulePass {
-	std::map< int, std::set<Value*> > finalValueSets;
-	std::map< int, std::set<int> > finalMemSets;
-	std::map<int, std::set<int> > intSets;
-	std::map<int, std::set<int> > intPointsTo;
-	bool runOnModule(Module &M);
-	void printSets();
-	void printIntSets(std::set<int> to_erase);
-	void mergeSets(int i, int j);
-	bool analyzeSets();
-	
+using namespace std;
+
+namespace llvm {
+	class AliasSets: public ModulePass {
+
+
+private:
+		llvm::DenseMap<int, int> disjointSet; // maps integers to the disjoint sets that contains the integer
+		llvm::DenseMap<int, std::set<int> > disjointSets; // table of disjoint sets
+
+		llvm::DenseMap<Value*, int> valueDisjointSet; // maps integers to the disjoint sets that contains the integer
+		llvm::DenseMap<int, std::set<Value*> > valueDisjointSets; // table of disjoint sets
+
+		bool runOnModule(Module &M);
+		void printSets();
+
 	public:
-	static char ID;
-	AliasSets();
-	void getAnalysisUsage(AnalysisUsage &AU) const;
-	std::map< int, std::set<Value*> > getValueSets();
-	std::map< int, std::set<int> > getMemSets();
-	int getValueSetKey(Value* v);
-	int getMapSetKey (int m);
-};
+		static char ID;
+		AliasSets() :
+				ModulePass(ID) {
+		}
+		;
+
+		void getAnalysisUsage(AnalysisUsage &AU) const;
+		llvm::DenseMap<int, std::set<Value*> > getValueSets();
+		llvm::DenseMap<int, std::set<int> > getMemSets();
+		int getValueSetKey(Value* v);
+		int getMapSetKey(int m);
+	};
+}
