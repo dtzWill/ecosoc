ab8e126cba09a35158e7c549204f4bdab3daa073
Henrique Santos <hnsantos@gmx.com>
2013-06-12 16:04:21 -0300

    Removed object files from ArAnot

--- src/ArAnot/Debug+Asserts/PointerAnalysis.d
+++
@@ -1,6 +0,0 @@
-/home/hnsantos/programs/llvm/llvm-3.3/lib/Transforms/ArAnot/Debug+Asserts/PointerAnalysis.o \
-  /home/hnsantos/programs/llvm/llvm-3.3/lib/Transforms/ArAnot/Debug+Asserts/PointerAnalysis.d: \
-  PointerAnalysis.cpp \
-  /home/hnsantos/programs/llvm/llvm-3.3/lib/Transforms/ArAnot/PointerAnalysis.h
-
-/home/hnsantos/programs/llvm/llvm-3.3/lib/Transforms/ArAnot/PointerAnalysis.h:
