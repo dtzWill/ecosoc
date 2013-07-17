ed5fa71a8d8bc674078dd857d47250767fca5cb5
brunors172 <brunors172@gmail.com>
2013-07-17 16:40:44 -0300

    included interator on graph class and changed the definition of generateSubgraph(). Moreove, I have fix the memory leak bug

--- src/DepGraph/DepGraph.h
+++ src/DepGraph/DepGraph.h
@@ -29,6 +29,7 @@
 #include <string>
 #include <sstream>
 #include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -157,6 +158,8 @@ public:
         }
         ;
         Function* getCalledFunction() const;
+	
+	CallInst* getCallInst() const;
 
         std::string getLabel();
         std::string getShape();
@@ -261,12 +264,22 @@ private:
 
 
 public:
+
+	typedef std::set<GraphNode*>::iterator iterator;
+
+        std::set<GraphNode*>::iterator begin();
+        std::set<GraphNode*>::iterator end();
+
         Graph(AliasSets *AS) :
                 AS(AS) {
                 NrEdges = 0;
         }
         ; //Constructor
         ~Graph(); //Destructor - Free adjacent matrix's memory
+
+	int getTaintedEdges ();
+        int getTaintedNodesSize ();
+
         GraphNode* addInst(Value *v); //Add an instruction into Dependence Graph
 
         void addEdge(GraphNode* src, GraphNode* dst, edgeType type = etData);
@@ -299,8 +312,8 @@ public:
 
         Graph generateSubGraph(Value *src, Value *dst); //Take a source value and a destination value and find a Connecting Subgraph from source to destination
 
-        void dfsVisit(GraphNode* u, std::set<GraphNode*> &visitedNodes); //Used by findConnectingSubgraph() method
-        void dfsVisitBack(GraphNode* u, std::set<GraphNode*> &visitedNodes); //Used by findConnectingSubgraph() method
+        void dfsVisit(GraphNode* u, GraphNode* u2, std::set<GraphNode*> &visitedNodes); //Used by findConnectingSubgraph() method
+        void dfsVisitBack(GraphNode* u, GraphNode* u2, std::set<GraphNode*> &visitedNodes); //Used by findConnectingSubgraph() method
 
         void deleteCallNodes(Function* F);
 
