8ca8234e0efcec0c57230c9bb2c0befff9274744
Bruno R Silva <brunors172@gmail.com>
2014-01-27 13:05:37 -0200

    bSSA2 added and Hammock bug fixed

---
+++ src/bSSA2/Debug+Asserts/bSSA.d
@@ -0,0 +1,309 @@
+/home/bruno/Dropbox/llvm/lib/Transforms/bSSA/Debug+Asserts/bSSA.o \
+ /home/bruno/Dropbox/llvm/lib/Transforms/bSSA/Debug+Asserts/bSSA.d: \
+ bSSA.cpp bSSA.h /home/bruno/Dropbox/llvm/include/llvm/Pass.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/Compiler.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Config/llvm-config.h \
+ /home/bruno/Dropbox/llvm/include/llvm/PassSupport.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Pass.h \
+ /home/bruno/Dropbox/llvm/include/llvm/InitializePasses.h \
+ /home/bruno/Dropbox/llvm/include/llvm/PassRegistry.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/StringRef.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/type_traits.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/DataTypes.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/CBindingWrapping.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/Casting.h \
+ /home/bruno/Dropbox/llvm/include/llvm-c/Core.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/Atomic.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/Valgrind.h \
+ /home/bruno/Dropbox/llvm/include/llvm/PassAnalysisSupport.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/SmallVector.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/AlignOf.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/MathExtras.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/SwapByteOrder.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/Statistic.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Analysis/Dominators.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/DenseMap.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/DenseMapInfo.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/PointerLikeTypeTraits.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/DepthFirstIterator.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/GraphTraits.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/PointerIntPair.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/SmallPtrSet.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/Function.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/Argument.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/Twine.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/ErrorHandling.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/ilist_node.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/Attributes.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/ArrayRef.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/None.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/FoldingSet.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/Value.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/Use.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/BasicBlock.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/ilist.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/Instruction.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/User.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/DebugLoc.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/Instruction.def \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/SymbolTableListTraits.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/CallingConv.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/GlobalValue.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/Constant.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/DerivedTypes.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/Type.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/APFloat.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/APInt.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/CFG.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/InstrTypes.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/OperandTraits.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/raw_ostream.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/FileSystem.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/IntrusiveRefCntPtr.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/OwningPtr.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/SmallString.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/TimeValue.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/system_error.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/Instructions.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/IntegersSubset.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/Constants.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/LLVMContext.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/IntegersSubsetMapping.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Analysis/DominanceFrontier.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Analysis/PostDominators.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/SetOperations.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Analysis/DominatorInternals.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Assembly/Writer.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/Debug.h \
+ ../DepGraph/DepGraph.h /home/bruno/Dropbox/llvm/include/llvm/Module.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/GlobalAlias.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/GlobalVariable.h \
+ /home/bruno/Dropbox/llvm/include/llvm/IR/Metadata.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/CallSite.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/CommandLine.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/StringMap.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/Allocator.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/GraphWriter.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/DOTGraphTraits.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/Path.h \
+ ../DepGraph/../AliasSets/AliasSets.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Function.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Instruction.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Analysis/AliasSetTracker.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/ValueHandle.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Analysis/AliasAnalysis.h \
+ /home/bruno/Dropbox/llvm/include/llvm/BasicBlock.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/InstIterator.h \
+ /home/bruno/Dropbox/llvm/include/llvm/PADriver.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Instructions.h \
+ /home/bruno/Dropbox/llvm/include/llvm/InstrTypes.h \
+ /home/bruno/Dropbox/llvm/include/llvm/PointerAnalysis.h \
+ /home/bruno/Dropbox/llvm/include/llvm/DebugInfo.h \
+ /home/bruno/Dropbox/llvm/include/llvm/Support/Dwarf.h \
+ /home/bruno/Dropbox/llvm/include/llvm/ADT/PostOrderIterator.h
+
+bSSA.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Pass.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/Compiler.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Config/llvm-config.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/PassSupport.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Pass.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/InitializePasses.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/PassRegistry.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/StringRef.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/type_traits.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/DataTypes.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/CBindingWrapping.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/Casting.h:
+
+/home/bruno/Dropbox/llvm/include/llvm-c/Core.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/Atomic.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/Valgrind.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/PassAnalysisSupport.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/SmallVector.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/AlignOf.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/MathExtras.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/SwapByteOrder.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/Statistic.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Analysis/Dominators.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/DenseMap.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/DenseMapInfo.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/PointerLikeTypeTraits.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/DepthFirstIterator.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/GraphTraits.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/PointerIntPair.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/SmallPtrSet.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/Function.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/Argument.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/Twine.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/ErrorHandling.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/ilist_node.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/Attributes.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/ArrayRef.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/None.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/FoldingSet.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/Value.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/Use.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/BasicBlock.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/ilist.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/Instruction.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/User.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/DebugLoc.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/Instruction.def:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/SymbolTableListTraits.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/CallingConv.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/GlobalValue.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/Constant.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/DerivedTypes.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/Type.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/APFloat.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/APInt.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/CFG.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/InstrTypes.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/OperandTraits.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/raw_ostream.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/FileSystem.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/IntrusiveRefCntPtr.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/OwningPtr.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/SmallString.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/TimeValue.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/system_error.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/Instructions.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/IntegersSubset.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/Constants.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/LLVMContext.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/IntegersSubsetMapping.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Analysis/DominanceFrontier.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Analysis/PostDominators.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/SetOperations.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Analysis/DominatorInternals.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Assembly/Writer.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/Debug.h:
+
+../DepGraph/DepGraph.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Module.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/GlobalAlias.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/GlobalVariable.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/IR/Metadata.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/CallSite.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/CommandLine.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/StringMap.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/Allocator.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/GraphWriter.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/DOTGraphTraits.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/Path.h:
+
+../DepGraph/../AliasSets/AliasSets.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Function.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Instruction.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Analysis/AliasSetTracker.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/ValueHandle.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Analysis/AliasAnalysis.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/BasicBlock.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/InstIterator.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/PADriver.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Instructions.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/InstrTypes.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/PointerAnalysis.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/DebugInfo.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/Support/Dwarf.h:
+
+/home/bruno/Dropbox/llvm/include/llvm/ADT/PostOrderIterator.h:
---
+++ src/bSSA2/Release+Asserts/bSSA.d
@@ -0,0 +1,299 @@
+/home/bruno/Dropbox/llvm33/lib/Transforms/bSSA/Release+Asserts/bSSA.o \
+  /home/bruno/Dropbox/llvm33/lib/Transforms/bSSA/Release+Asserts/bSSA.d: \
+  bSSA.cpp /home/bruno/Dropbox/llvm33/lib/Transforms/bSSA/bSSA.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Pass.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/Compiler.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Config/llvm-config.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/PassSupport.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/InitializePasses.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/PassRegistry.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/StringRef.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/type_traits.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/DataTypes.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/CBindingWrapping.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/Casting.h \
+  /home/bruno/Dropbox/llvm33/include/llvm-c/Core.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/Atomic.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/Valgrind.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/PassAnalysisSupport.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/SmallVector.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/AlignOf.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/MathExtras.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/SwapByteOrder.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/Statistic.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Analysis/Dominators.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/DenseMap.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/DenseMapInfo.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/PointerLikeTypeTraits.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/DepthFirstIterator.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/GraphTraits.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/PointerIntPair.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/SmallPtrSet.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/Function.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/Argument.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/Twine.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/ErrorHandling.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/ilist_node.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/Attributes.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/ArrayRef.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/None.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/FoldingSet.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/Value.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/Use.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/BasicBlock.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/ilist.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/Instruction.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/User.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/DebugLoc.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/Instruction.def \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/SymbolTableListTraits.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/CallingConv.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/GlobalValue.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/Constant.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/DerivedTypes.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/Type.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/APFloat.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/APInt.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/CFG.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/InstrTypes.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/OperandTraits.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/raw_ostream.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/Instructions.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/IntegersSubset.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/Constants.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/LLVMContext.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/IntegersSubsetMapping.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Analysis/DominanceFrontier.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Analysis/PostDominators.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/SetOperations.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Analysis/DominatorInternals.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Assembly/Writer.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/Debug.h \
+  /home/bruno/Dropbox/llvm33/lib/Transforms/bSSA/../DepGraph/DepGraph.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Module.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/OwningPtr.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/GlobalAlias.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/GlobalVariable.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/Metadata.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/CallSite.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/CommandLine.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/StringMap.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/Allocator.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/GraphWriter.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/DOTGraphTraits.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/Path.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/PathV1.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/TimeValue.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/PathV2.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/SmallString.h \
+  /home/bruno/Dropbox/llvm33/lib/Transforms/bSSA/../DepGraph/../AliasSets/AliasSets.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Analysis/AliasSetTracker.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/ValueHandle.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Analysis/AliasAnalysis.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/InstIterator.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/PADriver.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/PointerAnalysis.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/DebugInfo.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/Dwarf.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/ADT/PostOrderIterator.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Operator.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/IR/DataLayout.h \
+  /home/bruno/Dropbox/llvm33/include/llvm/Support/GetElementPtrTypeIterator.h
+
+/home/bruno/Dropbox/llvm33/lib/Transforms/bSSA/bSSA.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Pass.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/Compiler.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Config/llvm-config.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/PassSupport.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/InitializePasses.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/PassRegistry.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/StringRef.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/type_traits.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/DataTypes.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/CBindingWrapping.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/Casting.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm-c/Core.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/Atomic.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/Valgrind.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/PassAnalysisSupport.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/SmallVector.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/AlignOf.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/MathExtras.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/SwapByteOrder.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/Statistic.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Analysis/Dominators.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/DenseMap.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/DenseMapInfo.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/PointerLikeTypeTraits.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/DepthFirstIterator.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/GraphTraits.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/PointerIntPair.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/SmallPtrSet.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/Function.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/Argument.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/Twine.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/ErrorHandling.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/ilist_node.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/Attributes.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/ArrayRef.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/None.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/FoldingSet.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/Value.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/Use.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/BasicBlock.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/ilist.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/Instruction.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/User.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/DebugLoc.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/Instruction.def:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/SymbolTableListTraits.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/CallingConv.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/GlobalValue.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/Constant.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/DerivedTypes.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/Type.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/APFloat.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/APInt.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/CFG.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/InstrTypes.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/OperandTraits.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/raw_ostream.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/Instructions.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/IntegersSubset.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/Constants.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/LLVMContext.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/IntegersSubsetMapping.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Analysis/DominanceFrontier.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Analysis/PostDominators.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/SetOperations.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Analysis/DominatorInternals.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Assembly/Writer.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/Debug.h:
+
+/home/bruno/Dropbox/llvm33/lib/Transforms/bSSA/../DepGraph/DepGraph.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Module.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/OwningPtr.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/GlobalAlias.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/GlobalVariable.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/Metadata.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/CallSite.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/CommandLine.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/StringMap.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/Allocator.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/GraphWriter.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/DOTGraphTraits.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/Path.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/PathV1.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/TimeValue.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/PathV2.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/SmallString.h:
+
+/home/bruno/Dropbox/llvm33/lib/Transforms/bSSA/../DepGraph/../AliasSets/AliasSets.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Analysis/AliasSetTracker.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/ValueHandle.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Analysis/AliasAnalysis.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/InstIterator.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/PADriver.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/PointerAnalysis.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/DebugInfo.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/Dwarf.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/ADT/PostOrderIterator.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Operator.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/IR/DataLayout.h:
+
+/home/bruno/Dropbox/llvm33/include/llvm/Support/GetElementPtrTypeIterator.h:
